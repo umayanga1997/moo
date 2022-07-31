a63585e9b47bbd281dcafd9c983b9abe7e5e62a3
umayanga1997 <umayanga.avi@gmail.com>
2022-08-01 01:01:45 +0530

    implemented firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:moo/controllers/category_controller.dart';
@@ -5,7 +6,9 @@ import 'package:moo/controllers/language_controller.dart';
 import 'package:moo/layouts/layout.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
